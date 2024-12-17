6d34b6d8727c4dee3601e4363b35f570065dbdde
coloradude <steezeven@gmail.com>
2024-12-16 22:20:09 -0700

    build

--- csvToJson.js
+++ csvToJson.js
@@ -24,37 +24,39 @@ function csvToJson(csvContent) {
 }
 
 // Example usage:
-const csvContent = `
-Song,URL,Start,End
-Greased Lightening by Grease,https://www.youtube.com/watch?v=wK63eUyk-iM,0:30,1:00
-So Sick by Ne-yo,https://www.youtube.com/watch?v=IxszlJppRQI,0:20,0:52
-Rocky Mountain High by John Denver,https://www.youtube.com/watch?v=TtFwqnaPqXY,0:23,0:54
-Wide Open Spaces by The Chicks (Dixie Chicks),https://www.youtube.com/watch?v=dom7VlltBUc,0:38,1:13
-Blame it on the Rain by Milli Vanilli,https://www.youtube.com/watch?v=BI5IA8assfk,0:38,1:14
-Gimme Gimme Gimme by ABBA,https://www.youtube.com/watch?v=XEjLoHdbVeE&list=PLdB2qly-JTgD3_kIrWe04D-w6rHI0b2MW,0:13,0:46
-"No Woman, no Cry by Bob Marley",https://www.youtube.com/watch?v=IT8XvzIfi4U,1:59,2:35
-The Climb by Miley Cyrus,https://www.youtube.com/watch?v=NG2zyeVRcbs,0:37,1:06
-Someone Like you by Adele,https://www.youtube.com/watch?v=hLQl3WQQoQ0,0:45,1:12
-Hey Judy by the Beatles,https://www.youtube.com/watch?v=A_MjCqQoLLA&list=PLX_AKA6xDYIlK6wMqtv9dPQGjg3znDNVV&index=20,1:28,2:03
-I Miss you by Blink 182,https://www.youtube.com/watch?v=s1tAYmMjLdY,2:25,2:58
-Stolen Dance by Milky Chance,https://www.youtube.com/watch?v=iX-QaNzd-0Y,1:56,2:33
-Mountain Music by Alabama,https://www.youtube.com/watch?v=M6WfM0cXWSQ,1:07,1:47
-Billie Jean by Michael Jackson,https://www.youtube.com/watch?v=Zi_XLOBDo_Y,2:01,2:41
-Stick Season by Noah Kahan,https://www.youtube.com/watch?v=JKrDdsgXuso,1:07,1:39
-Cowboy Cassanova by Carrie Underwood,https://www.youtube.com/watch?v=oM7NQQ0Lfu4,2:18,2:48
-Levels by Avicii,https://www.youtube.com/watch?v=_ovdm2yX4MA,0:17,0:53
-Maria Maria by Santana,https://www.youtube.com/watch?v=nPLV7lGbmT4,1:46,2:18
-Bejeweled by Taylor Swift,https://www.youtube.com/watch?v=b7QlX3yR2xs,2:35,3:07
-Only Wanna Be with You by Hootie & The Blowfish,https://www.youtube.com/watch?v=Ln6WQqRDrCo&list=PL4EDF0448D3DB6031&index=34,0:35,1:12
-Sugar by Maroon 5,https://www.youtube.com/watch?v=09R8_2nJtjg,0:42,1:20
-Summertime Sadness by Lana Del Rey,https://www.youtube.com/watch?v=TdrL3QxjyVw,1:12,1:46
-Gettin' Jiggy Wit It by Will Smith,https://www.youtube.com/watch?v=3JcmQONgXJM&list=RDCLAK5uy_mpx6f-Md6iPbWSaizatzQIHfcF33fmT7E&index=27,0:50,1:10
-No One by Alicia Keys,https://www.youtube.com/watch?v=rywUS-ohqeE&list=PLJsUzVz-hbMJa7mhcBs9gP94omY0Y4Qha,2:52,3:26
-Chattahoochee by Alan Jackson,https://www.youtube.com/watch?v=JW5UEW2kYvc,0:54,1:22
-Material Girl by Madonna,https://www.youtube.com/watch?v=6p-lDYPR2P8&list=RDEMaN9C20MoM3K8E1iVi3CAmg&index=3,3:06,3:51
-Sam Love by Macklemore,https://www.youtube.com/watch?v=hlVBg7_08n0,4:19,4:46
-`;
+// const csvContent = `
+// Song,URL,Start,End
+// Greased Lightening by Grease,https://www.youtube.com/watch?v=wK63eUyk-iM,0:30,1:00
+// So Sick by Ne-yo,https://www.youtube.com/watch?v=IxszlJppRQI,0:20,0:52
+// Rocky Mountain High by John Denver,https://www.youtube.com/watch?v=TtFwqnaPqXY,0:23,0:54
+// Wide Open Spaces by The Chicks (Dixie Chicks),https://www.youtube.com/watch?v=dom7VlltBUc,0:38,1:13
+// Blame it on the Rain by Milli Vanilli,https://www.youtube.com/watch?v=BI5IA8assfk,0:38,1:14
+// Gimme Gimme Gimme by ABBA,https://www.youtube.com/watch?v=XEjLoHdbVeE&list=PLdB2qly-JTgD3_kIrWe04D-w6rHI0b2MW,0:13,0:46
+// "No Woman, no Cry by Bob Marley",https://www.youtube.com/watch?v=IT8XvzIfi4U,1:59,2:35
+// The Climb by Miley Cyrus,https://www.youtube.com/watch?v=NG2zyeVRcbs,0:37,1:06
+// Someone Like you by Adele,https://www.youtube.com/watch?v=hLQl3WQQoQ0,0:45,1:12
+// Hey Judy by the Beatles,https://www.youtube.com/watch?v=A_MjCqQoLLA&list=PLX_AKA6xDYIlK6wMqtv9dPQGjg3znDNVV&index=20,1:28,2:03
+// I Miss you by Blink 182,https://www.youtube.com/watch?v=s1tAYmMjLdY,2:25,2:58
+// Stolen Dance by Milky Chance,https://www.youtube.com/watch?v=iX-QaNzd-0Y,1:56,2:33
+// Mountain Music by Alabama,https://www.youtube.com/watch?v=M6WfM0cXWSQ,1:07,1:47
+// Billie Jean by Michael Jackson,https://www.youtube.com/watch?v=Zi_XLOBDo_Y,2:01,2:41
+// Stick Season by Noah Kahan,https://www.youtube.com/watch?v=JKrDdsgXuso,1:07,1:39
+// Cowboy Cassanova by Carrie Underwood,https://www.youtube.com/watch?v=oM7NQQ0Lfu4,2:18,2:48
+// Levels by Avicii,https://www.youtube.com/watch?v=_ovdm2yX4MA,0:17,0:53
+// Maria Maria by Santana,https://www.youtube.com/watch?v=nPLV7lGbmT4,1:46,2:18
+// Bejeweled by Taylor Swift,https://www.youtube.com/watch?v=b7QlX3yR2xs,2:35,3:07
+// Only Wanna Be with You by Hootie & The Blowfish,https://www.youtube.com/watch?v=Ln6WQqRDrCo&list=PL4EDF0448D3DB6031&index=34,0:35,1:12
+// Sugar by Maroon 5,https://www.youtube.com/watch?v=09R8_2nJtjg,0:42,1:20
+// Summertime Sadness by Lana Del Rey,https://www.youtube.com/watch?v=TdrL3QxjyVw,1:12,1:46
+// Gettin' Jiggy Wit It by Will Smith,https://www.youtube.com/watch?v=3JcmQONgXJM&list=RDCLAK5uy_mpx6f-Md6iPbWSaizatzQIHfcF33fmT7E&index=27,0:50,1:10
+// No One by Alicia Keys,https://www.youtube.com/watch?v=rywUS-ohqeE&list=PLJsUzVz-hbMJa7mhcBs9gP94omY0Y4Qha,2:52,3:26
+// Chattahoochee by Alan Jackson,https://www.youtube.com/watch?v=JW5UEW2kYvc,0:54,1:22
+// Material Girl by Madonna,https://www.youtube.com/watch?v=6p-lDYPR2P8&list=RDEMaN9C20MoM3K8E1iVi3CAmg&index=3,3:06,3:51
+// Sam Love by Macklemore,https://www.youtube.com/watch?v=hlVBg7_08n0,4:19,4:46
+// `;
+
+const csvContent = fs.readFileSync('mvb-sheet-2.csv', 'utf8')
 
 const jsonOutput = csvToJson(csvContent);
 
-fs.writeFileSync('mvb-json.json', JSON.stringify(jsonOutput, null, 2))
+fs.writeFileSync('mvb-json-2.json', JSON.stringify(jsonOutput, null, 2))
--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
-import { useState } from 'react'
-import songs from './mvb-json.json'
+import { useState } from 'react';
+import songs from './mvb-json.json';
+import secondSongs from './mvb-json-2.json';
 
 function generateYouTubeEmbedUrl({ videoUrl, startTime, endTime }) {
-  // Helper function to convert MM:SS to seconds
   function timeToSeconds(time) {
     const [minutes, seconds] = time.split(":").map(Number);
     return minutes * 60 + seconds;
   }
 
-  // Convert startTime and endTime to seconds
   const startSeconds = timeToSeconds(startTime);
   const endSeconds = timeToSeconds(endTime);
 
-  // Extract the video ID from the YouTube URL
   const urlParams = new URLSearchParams(new URL(videoUrl).search);
   const videoId = urlParams.get("v");
 
@@ -21,93 +19,83 @@ function generateYouTubeEmbedUrl({ videoUrl, startTime, endTime }) {
     throw new Error("Invalid YouTube URL");
   }
 
-  // Construct the embed URL with start and end times
-  const embedUrl = `https://www.youtube.com/embed/${videoId}?start=${startSeconds}&end=${endSeconds}&autoplay=1&rel=0`;
-  return embedUrl;
+  return `https://www.youtube.com/embed/${videoId}?start=${startSeconds}&end=${endSeconds}&autoplay=1&rel=0`;
 }
 
-
 function App() {
+  const [currentSongIndex, setCurrentSongIndex] = useState(undefined);
+  const [currentSongs, setCurrentSongs] = useState(songs); // State to manage active song list
 
-//   const songs = [{
-//     videoUrl: "https://www.youtube.com/watch?v=tUNbhYcY9Ik",
-//     startTime: "01:30", 
-//     endTime: "02:45",
-//     nameAndArtist: 'Troll Tongue - Kygo',
-//     hasNotBeenPlayed: true
-//   }, {
-//     videoUrl: 'https://www.youtube.com/watch?v=k85mRPqvMbE',
-//     startTime: "01:30", 
-//     endTime: "02:45",
-//     nameAndArtist: 'Crazy Frog - Annoying',
-//     hasNotBeenPlayed: true
-//   }
-// ]
-
-  const [currentSongIndex, setCurrentSongIndex] = useState(undefined)
-
-  // Function to get a random URL
-  // function getRandomSong() {
-  //   const randomIndex = Math.floor(Math.random() * songs.length);
-  //   if (songs[randomIndex].hasNotBeenPlayed){
-  //     songs[randomIndex].hasNotBeenPlayed = false
-  //     return songs[randomIndex]
-  //   } else {
-  //     getRandomSong()
-  //   }
-  // }
-
-  console.log(songs.length)
-
-  // Function to handle button click
   const handleNext = () => {
-    setCurrentSongIndex(currentSongIndex + 1);
+    setCurrentSongIndex((prevIndex) => prevIndex + 1);
   };
 
   const handlePrevious = () => {
-    setCurrentSongIndex(currentSongIndex - 1)
-  }
+    setCurrentSongIndex((prevIndex) => prevIndex - 1);
+  };
 
   const startGameClick = () => {
-    setCurrentSongIndex(0)
-  }
+    setCurrentSongIndex(0); // Start game from the first song
+  };
+
+  const startNewGameClick = () => {
+    setCurrentSongs(secondSongs); // Switch to secondSongs
+    setCurrentSongIndex(0); // Reset the index to 0
+  };
 
   const YoutubeFrame = () => {
-    return <div className='player-container'>
-      <div className="frosty-blur"></div>
-      <iframe
-        width="100%"
-        height="100%"
-        src={generateYouTubeEmbedUrl(songs[currentSongIndex])}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+    return (
+      <div className="player-container">
+        <div className="frosty-blur"></div>
+        <iframe
+          width="100%"
+          height="100%"
+          src={generateYouTubeEmbedUrl(currentSongs[currentSongIndex])}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
       </div>
-  }
+    );
+  };
 
   const StartGameButton = () => {
-    return <button onClick={startGameClick}>Start Game</button>
-  }
+    return <button onClick={startGameClick}>Start Game</button>;
+  };
 
   return (
-    <div style={{ textAlign: "center", height: '100%' }}>
+    <div style={{ textAlign: 'center', height: '100%' }}>
+      <span>{`${currentSongIndex !== undefined ? currentSongIndex + 1 : 1} / ${currentSongs.length}`}</span>
       <h1>🧠 BIAC Music Video Bingo 🎶</h1>
-      {/* {currentSongIndex >= songs.length ? <GameOver /> : ''} */}
-      {currentSongIndex >= 0 && currentSongIndex < songs.length ? <YoutubeFrame /> : <StartGameButton />}
-      
-      <div>
-        
-      </div>
-      <br />
-      <button onClick={handlePrevious} style={{ marginTop: "20px", padding: "10px 20px", fontSize: "16px" }}>
-      ⏪ Previous Video
+
+      {currentSongIndex !== undefined &&
+      currentSongIndex >= 0 &&
+      currentSongIndex < currentSongs.length ? (
+        <YoutubeFrame />
+      ) : (
+        <StartGameButton />
+      )}
+
+
+      <button
+        onClick={handlePrevious}
+        style={{ marginTop: '20px', padding: '10px 20px', fontSize: '16px' }}
+        disabled={currentSongIndex <= 0}
+      >
+        ⏪ Previous Video
       </button>
-      <button onClick={handleNext} style={{ marginTop: "20px", padding: "10px 20px", fontSize: "16px" }}>
+      <button
+        onClick={handleNext}
+        style={{ marginTop: '20px', padding: '10px 20px', fontSize: '16px' }}
+        disabled={currentSongIndex === undefined || currentSongIndex >= currentSongs.length - 1}
+      >
         Next Video ⏩
       </button>
-      <button onClick={handleNext} style={{ marginTop: "20px", padding: "10px 20px", fontSize: "16px" }}>
+      <button
+        onClick={startNewGameClick}
+        style={{ marginTop: '20px', padding: '10px 20px', fontSize: '16px' }}
+      >
         New Game 👶
       </button>
     </div>
